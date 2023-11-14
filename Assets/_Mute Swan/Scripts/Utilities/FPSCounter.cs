df2bf43e775f5184e83291f6a523e989a486e2e1
henrymik <113172684+hemi2201@users.noreply.github.com>
2023-11-14 10:57:23 +0100

    Update Main Scene
    - Remove redundant XR Interaction Manager
    - Add utilities
    - Update FPS Counter position and require Content Size Fitter

--- Assets/_Mute Swan/Scripts/Utilities/FPSCounter.cs
+++ Assets/_Mute Swan/Scripts/Utilities/FPSCounter.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 namespace Utilities
 {
-    [RequireComponent(typeof(TextMeshPro))]
+    [RequireComponent(typeof(TextMeshPro), typeof(ContentSizeFitter))]
     public class FPSCounter : MonoBehaviour
     {
         private string fpsText;
